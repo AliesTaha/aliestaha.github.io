783394a343a1199875022cefbdf5d3a0788bf21e
Ali Taha <aliestaha04@gmail.com>
2022-12-22 01:07:02 -0500

    Links and x button fixed
    I'm a genius...what can I say.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,19 +5,34 @@ const navMenu=document.getElementById('nav-menu'),
 
 /*Show Menu, again :(*/
 if(navToggle){
+    //I just learned how to use arrow functions: too cool to not use.
     navToggle.addEventListener('click', ()=>{
         navMenu.classList.add('show-menu')
     })
     //now that I made show menu, I will introduce its style in css
 }
 
-
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
+/*Hide the menu, again*/
+if(navClose){
+    //define an arrow function 
+    navClose.addEventListener('click', ()=> 
+    {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
 
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+
+    //Removing the show-menu class
+    
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n=>n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 
