2d8100f8c0e8fbc5d69c039b196910e002a1f13d
Ali Taha <aliestaha04@gmail.com>
2022-12-22 19:29:15 -0500

    Qualifications and Education
    Trying to implement these sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,8 +34,24 @@ function linkAction(){
 }
 navLink.forEach(n=>n.addEventListener('click', linkAction))
 
-/*==================== ACCORDION SKILLS ====================*/
+/*Skills*/
+const skillsContent=document.getElementsByClassName('skills_content')
+    skillsHeader=document.querySelectorAll('.skills_header')
 
+function toggleSkills(){
+    let itemClass=this.parentNode.className
+
+    for(i=0; i<skillsContent.length;i++){
+        skillsContent[i].className='skills_content skills_close'
+    }
+    if(itemClass=='skills_content skills_close'){
+        this.parentNode.className='skills_content skills_open'
+    }
+}
+
+skillsHeader.forEach((eL)=>{
+    eL.addEventListener('click', toggleSkills)
+})
 
 /*==================== QUALIFICATION TABS ====================*/
 
