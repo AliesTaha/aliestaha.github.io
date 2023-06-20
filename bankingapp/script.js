0bba76f3755f14f4b184bf6647fce02b574c63e8
Ali Taha <ataha.blackberry.com>
2023-06-20 13:56:25 -0400

    fixed sorting button

--- bankingapp/script.js
+++ bankingapp/script.js
@@ -61,9 +61,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements){
+const displayMovements = function (movements, sort=false){
   document.querySelector('.movements').innerHTML='';
-  movements.forEach(function (mov, i){
+
+  const movs=sort ? movements.slice().sort((a,b)=> a-b) : movements;
+
+  movs.forEach(function (mov, i){
     const type = (mov>0)? 'deposit' : 'withdrawal';
     const html=`
     <div class="movements__row">
@@ -306,4 +309,11 @@ btnClose.addEventListener('click', (e)=>{
   }
   inputCloseUsername.value=inputClosePin.value='';
 
+});
+
+let sorted= false;
+btnSort.addEventListener('click', (e)=>{
+e.preventDefault();
+displayMovements(currentAccount.movements, !sorted);
+sorted=!sorted;
 });
